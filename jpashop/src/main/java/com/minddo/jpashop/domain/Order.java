e063890a99c053a734f8a43052ade977c13f7a38
Minhyung <mindrome95@kaist.ac.kr>
2021-09-23 16:49:03 +0900

    order service

--- jpashop/src/main/java/com/minddo/jpashop/domain/Order.java
+++ jpashop/src/main/java/com/minddo/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package com.minddo.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,8 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class Order {
 
     @Id @GeneratedValue
@@ -31,7 +35,7 @@ public class Order {
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus status; // 주문상태태
+    private OrderStatus status; // 주문상태
 
     //==연관관계 편의 메서드==//
     public void setMember(Member member) {
@@ -47,6 +51,47 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송된  상품은 취소가 불가능합니다");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+        return totalPrice;
     }
 
 }
---
+++ jpashop/src/test/java/com/minddo/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,110 @@
+package com.minddo.jpashop.service;
+
+import com.minddo.jpashop.domain.Address;
+import com.minddo.jpashop.domain.Member;
+import com.minddo.jpashop.domain.Order;
+import com.minddo.jpashop.domain.OrderStatus;
+import com.minddo.jpashop.domain.item.Book;
+import com.minddo.jpashop.domain.item.Item;
+import com.minddo.jpashop.exception.NoEnoughStockException;
+import com.minddo.jpashop.repository.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired OrderService orderService;
+    @Autowired OrderRepository orderRepository;
+
+
+    @Test
+    public void 상품주문() throws Exception {
+        //given
+        Member member = createMember();
+        Book book = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+
+        //when
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertThat(getOrder.getStatus()).isEqualTo(OrderStatus.ORDER);
+        assertThat(getOrder.getOrderItems().size()).isEqualTo(1);
+        assertThat(getOrder.getTotalPrice()).isEqualTo(10000*orderCount);
+        assertThat(book.getStockQuantity()).isEqualTo(8);
+
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        //given
+        Member member = createMember();
+        Book book = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        //when
+        orderService.cancelOrder(orderId);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertThat(getOrder.getStatus()).isEqualTo(OrderStatus.CANCEL);
+        assertThat(book.getStockQuantity()).isEqualTo(10);
+
+
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception{
+        //given
+        Member member = createMember();
+
+        Book book = createBook("시골 JPA", 10000,10);
+
+        int orderCount = 11;
+
+        //when
+        try {
+            Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+        } catch (NoEnoughStockException e){
+            return;
+        }
+        //then
+        fail("재고 수량 부족 예외가 발생해야 한다");
+    }
+
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "경기", "123-123"));
+        em.persist(member);
+        return member;
+    }
+
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
+
+}
