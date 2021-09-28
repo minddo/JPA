cfbeae60561122aab1b9c1b4e53a7c93cb336b25
Minhyung <mindrome95@kaist.ac.kr>
2021-09-28 18:00:15 +0900

    read order

---
+++ jpashop/src/main/java/com/minddo/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,72 @@
+package com.minddo.jpashop.api;
+
+import com.minddo.jpashop.domain.Address;
+import com.minddo.jpashop.domain.Order;
+import com.minddo.jpashop.domain.OrderStatus;
+import com.minddo.jpashop.repository.OrderRepository;
+import com.minddo.jpashop.repository.OrderSearch;
+import com.minddo.jpashop.repository.OrderSimpleQueryDto;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("api/v1/sample-orders")
+    public List<Order> orderV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        return all;
+    }
+
+    @GetMapping("api/v2/sample-orders")
+    public List<SimpleOrderDto> orderV2() {
+        // N + 1 문제 발생, fetch join 필요
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> collect = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @GetMapping("api/v3/sample-orders")
+    public List<SimpleOrderDto> orderV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(new OrderSearch());
+        List<SimpleOrderDto> collect = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @GetMapping("api/v4/sample-orders")
+    public List<OrderSimpleQueryDto> orderV4() {
+        // 원하는 column만 선택하여 가져올 수 있음
+        return orderRepository.findOrderDto();
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
+    }
+
+}
