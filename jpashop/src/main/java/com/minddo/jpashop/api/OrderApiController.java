90b0731a401af8b35b1eabb85182007526c2fb61
Minhyung <mindrome95@kaist.ac.kr>
2021-09-30 17:51:38 +0900

    read using dto

--- jpashop/src/main/java/com/minddo/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/com/minddo/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import com.minddo.jpashop.domain.OrderItem;
 import com.minddo.jpashop.domain.OrderStatus;
 import com.minddo.jpashop.repository.OrderRepository;
 import com.minddo.jpashop.repository.OrderSearch;
+import com.minddo.jpashop.repository.order.query.OrderQueryDto;
+import com.minddo.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("api/v1/orders")
     public List<Order> ordersV1() {
@@ -65,6 +68,11 @@ public class OrderApiController {
                 .collect(Collectors.toList());
 
         return collect;
+    }
+
+    @GetMapping("api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOderQueryDtos();
     }
 
     @Data
